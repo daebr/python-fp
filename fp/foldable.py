1a73b62824068cf2e5769cde77a1bcc099c6afd0
Ben Price <daebr.77@gmail.com>
2020-07-27 22:03:29 +1000

    flesh out foldable

--- fp/foldable.py
+++ fp/foldable.py
@@ -1,5 +1,20 @@
+from fp.core import const
+
+
 class Foldable:
 
     def fold(self, f, z):
         """(t a, ((a, b) -> b), b) -> b"""
         pass
+
+    def length(self):
+        """t a -> int"""
+        return self.fold(lambda a, z: z + 1, 0)
+
+    def isEmpty(self):
+        """t a -> bool"""
+        return self.fold(lambda a, b: False, True)
+
+    def contains(self, a):
+        """(t a, a) -> bool"""
+        return self.fold(lambda av, z: z or av == a, False)
